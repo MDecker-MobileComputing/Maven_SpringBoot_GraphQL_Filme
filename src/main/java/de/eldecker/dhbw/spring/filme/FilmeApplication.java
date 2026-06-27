9075c8c6d04ceaef36c0931a60139f87fa83e62b
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-27 15:52:14 +0200

    Vorlage von start.spring.io heruntergeladen https://start.spring.io/#!type=maven-project&language=java&platformVersion=4.1.0&packaging=jar&configurationFileFormat=properties&jvmVersion=21&groupId=de.eldecker.dhbw.spring&artifactId=filme&packageName=de.eldecker.dhbw.spring.filme&dependencies=graphql,web

---
+++ src/main/java/de/eldecker/dhbw/spring/filme/FilmeApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.filme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmeApplication.class, args);
+	}
+
+}
